dec79446222b2b7e2ce92e2586229d2601484969
laura.szekeres <szekereslaura98@gmail.com>
2024-07-31 15:58:47 +0200

    Adding the function of multiple, parallelizable runs of the GA
    The Evoluton class has been expanded with the method of running the Genetic Algorithm multiple times.

--- gitsbe/__init__.py
+++ gitsbe/__init__.py
@@ -21,30 +21,29 @@ if __name__ == '__main__':
 
     # BooleanModel init from .bnet file
     boolean_model_bnet = BooleanModel(file='../example_model_args/toy_ags2_equations.bnet', model_name='test')
-    boolean_model_bnet.generate_mutated_lists(5, 1)
 
     # BooleanModel init from .sif file
     boolean_model_sif = BooleanModel(model=interaction, model_name='test2')
-    boolean_model_sif.generate_mutated_lists(5, 1)
 
     # init pygad.GA
     ga_args = {
-        'num_generations': 30,
+        'num_generations': 20,
         'num_parents_mating': 2,
-        'fitness_batch_size': 10,
-        'sol_per_pop': 100,
+        'sol_per_pop': 10,
         'num_genes': 10,
         'parent_selection_type': "sss",
         'crossover_type': "single_point",
         'mutation_percent_genes': 40,
         'population_size': 20,
-        'number_of_mutations': 5,
-        'parallel_processing': 12
+        'number_of_mutations': 3,
+        'parallel_processing': 4
     }
     start_time = time.time()
     evolution = Evolution(boolean_model=boolean_model_bnet, training_data=training_data,
-                          model_outputs=model_outputs, ga_args=ga_args)
+                          model_outputs=model_outputs, num_best_solutions=3, num_mutations=5,
+                          num_mutations_per_eq=3, num_runs=20, num_cores=4, ga_args=ga_args)
     evolution.run()
+
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Total runtime: {elapsed_time} seconds")
--- gitsbe/model/Evolution.py
+++ gitsbe/model/Evolution.py
@@ -1,23 +1,34 @@
 import numpy as np
 import pygad
 from gitsbe.model.BooleanModelOptimizer import BooleanModelOptimizer
+import concurrent.futures
+import multiprocessing
 
 
 class Evolution(BooleanModelOptimizer):
-    def __init__(self, boolean_model=None, training_data=None, model_outputs=None, ga_args=None):
+    def __init__(self, boolean_model=None, training_data=None, model_outputs=None, ga_args=None,
+                 num_best_solutions=3, num_mutations=3, num_mutations_per_eq=3, num_runs=10, num_cores=None):
         """
         Initializes the Evolution class with a BooleanModel and genetic algorithm parameters.
         :param boolean_model: The boolean model to be evolved.
         :param ga_args: Dictionary containing all necessary arguments for pygad.
+        :param num_best_solutions: Number of the best solutions to track.
+        :param num_mutations: Number of the initial mutated equations.
+        :param num_mutations_per_eq: Number of the initial mutations per equation.
+        :param num_runs: Number of times to run the genetic algorithm.
+        :param num_cores: Number of cores to use for parallel execution.
         """
         self._boolean_model = boolean_model
         self._ga_args = ga_args or {}
         self._best_models = []
         self.mutation_number = self._ga_args.get('number_of_mutations')
-        self._initial_population = boolean_model.generate_mutated_lists(10, 3)
+        self._initial_population = boolean_model.generate_mutated_lists(num_mutations, num_mutations_per_eq)
         self._mutation_type = boolean_model.mutation_type
         self._training_data = training_data
         self._model_outputs = model_outputs
+        self._num_best_solutions = num_best_solutions
+        self._num_runs = num_runs
+        self._num_cores = num_cores if num_cores is not None else multiprocessing.cpu_count()
 
         if self._training_data is None or self._model_outputs is None:
             raise ValueError('Please provide training data and model outputs.')
@@ -105,15 +116,11 @@ class Evolution(BooleanModelOptimizer):
         return (pred_global_output - self._model_outputs.min_output) / (
                     self._model_outputs.max_output - self._model_outputs.min_output)
 
-    def callback_generation(self, ga_instance, num_best_solutions):
-        # Extract the population and their fitness scores
+    def callback_generation(self, ga_instance):
         population = ga_instance.population
         fitness = ga_instance.last_generation_fitness
-
-        # Get the indices of the sorted fitness values in descending order
         sorted_indices = np.argsort(fitness)[::-1]
 
-        # Initialize a set to track unique solutions and a list for new best models
         unique_solutions = set()
         new_best_models = []
 
@@ -122,38 +129,48 @@ class Evolution(BooleanModelOptimizer):
             if solution_tuple not in unique_solutions:
                 unique_solutions.add(solution_tuple)
                 new_best_models.append((population[idx], fitness[idx]))
-            if len(unique_solutions) == num_best_solutions:
+            if len(unique_solutions) == self._num_best_solutions:
                 break
 
-        # Sort the best solutions based on fitness values in descending order
         new_best_models.sort(key=lambda x: x[1], reverse=True)
+        self._best_models[:] = new_best_models[:self._num_best_solutions]
 
-        # Update the best models, ensuring only the specified number of top solutions
-        self._best_models[:] = new_best_models[:num_best_solutions]
-
-    def run(self):
-        initial_mutated_population = self._initial_population
+    def run_single_ga(self, evolution_number):
         ga_instance = pygad.GA(
             num_generations=self._ga_args.get('num_generations'),
             num_parents_mating=self._ga_args.get('num_parents_mating'),
             fitness_func=self.calculate_fitness,
-            fitness_batch_size=self._ga_args.get('fitness_batch_size', len(initial_mutated_population)),
+            fitness_batch_size=len(self._initial_population),
             sol_per_pop=self._ga_args.get('sol_per_pop'),
             gene_space=[0, 1],
-            num_genes=len(initial_mutated_population),
-            initial_population=initial_mutated_population,
+            num_genes=len(self._initial_population),
+            initial_population=self._initial_population,
             parent_selection_type=self._ga_args.get('parent_selection_type', "sss"),
             crossover_type=self._ga_args.get('crossover_type', "single_point"),
             mutation_type='random',
             mutation_percent_genes=self._ga_args.get('mutation_percent_genes'),
             parallel_processing=self._ga_args.get('parallel_processing'),
-            on_generation=lambda ga_instance: self.callback_generation(ga_instance, 3)
+            on_generation=self.callback_generation
         )
 
         ga_instance.run()
-        for idx, (solution, fitness) in enumerate(self._best_models):
-            print(f"Solution {idx + 1}: {solution}, Fitness: {fitness}")
 
-        # solution, solution_fitness, solution_idx = ga_instance.best_solution()
-        # print(f"Best solution: {solution}")
-        # print(f"Best solution fitness: {solution_fitness}")
+        best_models = self._best_models[:self._num_best_solutions]
+        # for idx, (solution, fitness) in enumerate(best_models):
+        #     print(f"Best {idx + 1} result of evolution {evolution_number}: solution: {solution}, fitness: {fitness}")
+
+        return best_models
+
+    def run(self):
+        all_best_models = []
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self._num_cores) as executor:
+            futures = [executor.submit(self.run_single_ga, i + 1) for i in range(self._num_runs)]
+            for future in concurrent.futures.as_completed(futures):
+                all_best_models.extend(future.result())
+
+        all_best_models.sort(key=lambda x: x[1], reverse=True)
+        self._best_models = all_best_models[:self._num_best_solutions]
+
+        print('\nBest solutions across all evolutions: ')
+        for idx, (solution, fitness) in enumerate(self._best_models):
+            print(f"\nBest Solution {idx + 1}: {solution}, Fitness: {fitness}")
