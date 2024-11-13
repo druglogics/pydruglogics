896bb309e76db6f347643c494183b543a0ea65ea
laura.szekeres <szekereslaura98@gmail.com>
2024-11-13 13:24:15 +0100

    Add githubpages extension

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ author = 'Laura Szekeres'
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
 ]
 autoclass_content = 'both'
 templates_path = ['_templates']
